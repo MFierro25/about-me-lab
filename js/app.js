75e20e065e520fba998e30eda97ab2b5ff5647a6
Miguel Fierro <emfierro2@gmail.com>
2021-09-15 23:32:43 -0700

    index.html

--- js/app.js
+++ js/app.js
@@ -1,38 +1,58 @@
 'use strict';
 
+
 let name = prompt('Hello what is your name?')
 
 alert('Welcome ' + name + ' lets play a guessing game about me.');
-alert('All the following questions will be yes or no and after the game is completed you will be able to read My About Me to see if you were right')
+alert('You will be scored on this game so take your best guess!')
 
 let military = prompt('Have i ever been in the military?').toLowerCase()
-if (military === 'yes') 
+if (military === 'yes' || 'y') 
     alert('Correct, i served 4 years in San Diego!')
-else if (military === 'no')
+else if (military === 'no' || 'n')
     alert('Actually, i did, i served 4 years in San Diego.')
 
 let oldest = prompt('Am i the youngest child?').toLowerCase()
-if (oldest === 'yes')
+if (oldest === 'yes' || 'y')
     alert('No, i am actually the oldest of Four.')
-    else if (oldest === 'no')
-    alert('Correct! i am actually the oldest of Four')
+    else if (oldest === 'no' || 'n')
+    alert('Correct! i am the oldest of Four')
+    
 
 let instrument = prompt('Can i play the Piano?').toLowerCase()
-if (instrument === 'yes')
+if (instrument === 'yes' || 'y')
     alert('No, i actually play the guitar and bass.')
-    else if (instrument === 'no')
+    else if (instrument === 'no' || 'n')
     alert('Correct, even though it looks fun i only know Guitar and Bass')
+   
 
 let country = prompt('Have i ever been outside of the country?').toLowerCase()
-if (country === 'yes')
+if (country === 'yes' || 'y')
     alert('CORRECT! ive actually been to 4 different countries')
-    else if (country === 'no')
+    else if (country === 'no' || 'n')
     alert('Actually i have, i have been to 4 different countries')
 
 let schools = prompt('Did i go to over 9 different schools growing up?').toLowerCase()
-if (schools === 'yes')
+if (schools === 'yes' || 'y')
     alert('YES! unfortunately i went to 10 different schools growing up.')
-    else if (military === 'no')
+    else if (schools === 'no' || 'n')
     alert('unfortunately i did, i went to 10 different schools growing up')
+       
+      let age= ('29')
+            let ageguess = prompt('How old do you think i am?')
+        if (ageguess === '29')
+            alert('Correct! i am 29')
+        else if(ageguess >'29') 
+            alert('Guess Lower Next Time!')
+        else if(ageguess < '29')
+            alert('Guess Higher Next Time!')
 
-alert('Well that was the whole game! you did a great job ' + name + ' welcome to the page to learn more about me.')
+   let favefood = []
+   favefood.push('teriyaki', 'pizza', 'chipotle', 'tacos');
+   console.log(favefood);
+    let foodguess = prompt('Whats one of my favorite foods?').toLowerCase()
+        if(foodguess == favefood){
+                alert('Yes that is one of my faves!')
+                userpoints ++;}
+        else if (foodguess != favefood)
+                alert('No, not that, try again')
