d7b43c757a966ddc605e697dbd83847e504853a8
Miguel Fierro <emfierro2@gmail.com>
2021-09-16 14:19:41 -0700

    fixed loops, added amount of guesses allowed

--- js/app.js
+++ js/app.js
@@ -1,58 +1,101 @@
 'use strict';
 
 
+let totalcorrect = 0
 let name = prompt('Hello what is your name?')
 
 alert('Welcome ' + name + ' lets play a guessing game about me.');
 alert('You will be scored on this game so take your best guess!')
 
 let military = prompt('Have i ever been in the military?').toLowerCase()
-if (military === 'yes' || 'y') 
-    alert('Correct, i served 4 years in San Diego!')
-else if (military === 'no' || 'n')
-    alert('Actually, i did, i served 4 years in San Diego.')
+    if (military === 'yes' || military === 'y') {
+        alert('CORRECT! i served 4 years in San Diego!')
+        totalcorrect ++;}
+    else if (military === 'no' || military === 'n')
+        alert('Incorrect, i served 4 years in San Diego.')
 
 let oldest = prompt('Am i the youngest child?').toLowerCase()
-if (oldest === 'yes' || 'y')
-    alert('No, i am actually the oldest of Four.')
-    else if (oldest === 'no' || 'n')
-    alert('Correct! i am the oldest of Four')
-    
+    if (oldest === 'yes' || oldest === 'y')
+        alert('Incorrect, i am actually the oldest of Four.')
+    else if (oldest === 'no' || oldest === 'n') {
+        alert('CORRECT! i am the oldest of Four')
+        totalcorrect ++; }
 
 let instrument = prompt('Can i play the Piano?').toLowerCase()
-if (instrument === 'yes' || 'y')
-    alert('No, i actually play the guitar and bass.')
-    else if (instrument === 'no' || 'n')
-    alert('Correct, even though it looks fun i only know Guitar and Bass')
-   
+    if (instrument === 'yes' || instrument === 'y')
+        alert('Incorrect, i actually play the guitar and bass.')
+    else if (instrument === 'no' || instrument === 'n') {
+        alert('CORRECT! even though it looks fun i only know Guitar and Bass')
+        totalcorrect ++;}
 
 let country = prompt('Have i ever been outside of the country?').toLowerCase()
-if (country === 'yes' || 'y')
-    alert('CORRECT! ive actually been to 4 different countries')
-    else if (country === 'no' || 'n')
-    alert('Actually i have, i have been to 4 different countries')
+    if (country === 'yes' || country === 'y') {
+        alert('CORRECT! ive actually been to 4 different countries')
+        totalcorrect ++;}
+    else if (country === 'no' || country === 'n')
+        alert('Incorrect i have, i have been to 4 different countries')
 
 let schools = prompt('Did i go to over 9 different schools growing up?').toLowerCase()
-if (schools === 'yes' || 'y')
-    alert('YES! unfortunately i went to 10 different schools growing up.')
-    else if (schools === 'no' || 'n')
-    alert('unfortunately i did, i went to 10 different schools growing up')
-       
-      let age= ('29')
-            let ageguess = prompt('How old do you think i am?')
-        if (ageguess === '29')
-            alert('Correct! i am 29')
-        else if(ageguess >'29') 
-            alert('Guess Lower Next Time!')
-        else if(ageguess < '29')
-            alert('Guess Higher Next Time!')
+    if (schools === 'yes' || schools === 'y') {
+        alert('CORRECT! unfortunately i went to 10 different schools growing up.')
+        totalcorrect ++;}
+    else if (schools === 'no' || schools === 'n')
+        alert('Incorrect, i went to 10 different schools growing up')
 
-   let favefood = []
-   favefood.push('teriyaki', 'pizza', 'chipotle', 'tacos');
-   console.log(favefood);
-    let foodguess = prompt('Whats one of my favorite foods?').toLowerCase()
-        if(foodguess == favefood){
-                alert('Yes that is one of my faves!')
-                userpoints ++;}
-        else if (foodguess != favefood)
-                alert('No, not that, try again')
+let age= ('29')
+let numbguess = 4;
+let ageguess = '' 
+let useriscorrect = false;	
+
+for (let i = 0; i < numbguess; i++) {
+    ageguess = prompt('How old do you think i am?')
+    if (ageguess == age) {
+        useriscorrect = true
+        alert('Correct! i am 29!')
+        totalcorrect ++;
+        break;
+    } else if(ageguess > '29') {
+        alert('Guess Lower Next Time!')
+    } else if(ageguess < '29') {
+        alert('Guess Higher Next Time!')
+    }
+}
+
+if (!useriscorrect) {
+    alert('You are out of guesses')
+} else {
+    alert('Great Job on the guess')
+}
+
+
+let favefood = ['teriyaki', 'pizza', 'chipotle', 'tacos'];
+let foodguess = 6;
+let correct = false;
+
+while(foodguess > 0 && correct == false) {  
+
+  let userfood = prompt('What is one of my favorite Foods?');
+
+  
+  for (let i = 0; i < foodguess.length; i++) {
+    if (userfood.toLowerCase() == favefood[i].toLowerCase()) {
+      alert('thats correct');
+      totalCorrect++;
+      correct = true;
+      break;
+    }
+  }
+
+  if (!correct) {
+    foodguess--;
+    alert('Try Again!');
+  }
+}
+
+if (!correct) {
+  alert('You ran out of guesses', favefood);
+} else {
+  alert('Great Job');
+}
+
+alert('Thanks for participating you got ' + totalcorrect + ' Questions right!')
