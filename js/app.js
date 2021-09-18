9b44ff83823cf0d44b4c30c11db062ed4b7d65d5
Miguel Fierro <emfierro2@gmail.com>
2021-09-17 17:43:26 -0700

    finished up final changes to script and index

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 let totalcorrect = 0
 let name = prompt('Hello what is your name?')
 
@@ -42,60 +41,21 @@ let schools = prompt('Did i go to over 9 different schools growing up?').toLower
     else if (schools === 'no' || schools === 'n')
         alert('Incorrect, i went to 10 different schools growing up')
 
-let age= ('29')
-let numbguess = 4;
-let ageguess = '' 
-let useriscorrect = false;	
-
-for (let i = 0; i < numbguess; i++) {
-    ageguess = prompt('How old do you think i am?')
-    if (ageguess == age) {
-        useriscorrect = true
-        alert('Correct! i am 29!')
-        totalcorrect ++;
-        break;
-    } else if(ageguess > '29') {
-        alert('Guess Lower Next Time!')
-    } else if(ageguess < '29') {
-        alert('Guess Higher Next Time!')
-    }
-}
-
-if (!useriscorrect) {
-    alert('You are out of guesses')
-} else {
-    alert('Great Job on the guess')
-}
-
-
 let favefood = ['teriyaki', 'pizza', 'chipotle', 'tacos'];
 let foodguess = 6;
 let correct = false;
 
-while(foodguess > 0 && correct == false) {  
-
-  let userfood = prompt('What is one of my favorite Foods?');
-
-  
-  for (let i = 0; i < foodguess.length; i++) {
-    if (userfood.toLowerCase() == favefood[i].toLowerCase()) {
-      alert('thats correct');
-      totalCorrect++;
-      correct = true;
-      break;
-    }
-  }
-
-  if (!correct) {
-    foodguess--;
-    alert('Try Again!');
-  }
+let userfood = prompt('What is one of my favorite Foods?'); {
+for (let i =0; i < foodguess; i++) {
+     if (userfood.toLowerCase() === favefood[i].toLowerCase()) {
+        alert('Thats Correct!');
+        totalcorrect++;
+        correct = true;
+        break;
+    }   else if (userfood.toLowerCase() != favefood[i].toLowerCase()); {
+         alert('That is incorrect try again!');
+     }
 }
-
-if (!correct) {
-  alert('You ran out of guesses', favefood);
-} else {
-  alert('Great Job');
 }
 
 alert('Thanks for participating you got ' + totalcorrect + ' Questions right!')
